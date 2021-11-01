36f80ac5bd895d38e331b0d8f1bf79d9df9653f9
Kuba <jciesmail@gmail.com>
2021-11-01 22:56:02 +0100

    simple cards application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cards';
+export class AppComponent 
+{
+	posts = [
+		{
+			title: 'Neat Tree',
+			imageUrl: 'assets/tree.jpeg',
+			username: 'nature',
+			content: 'I saw this neat tree today'
+		},
+		{
+			title: 'Snowy Mountain',
+			imageUrl: 'assets/mountain.jpeg',
+			username: 'mountaionlover',
+			content: 'Here is a picture of a snowy mountain'
+		},
+		{
+			title: 'Mountain Biking',
+			imageUrl: 'assets/biking.jpeg',
+			username: 'biking12222',
+			content: 'I did some biking today'
+		}
+	];
 }
